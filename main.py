5c0c69e224b8e4e0f745b6994c987e1aacf0a5e1
Frixiol <79292185+Frixiol@users.noreply.github.com>
2021-02-19 02:41:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import kivy
+from kivy.app import App
+from kivy.uix.screenmanager import  Screen
+from kivy.core.window import Window
+
+Window.size = (270, 585)
+
+class MainScreen(Screen):
+
+    def btn(self):
+        pass
+
+class MyApp(App):
+
+    def build(self):
+        return MainScreen()
+
+if __name__ ==  "__main__":
+    MyApp().run()
